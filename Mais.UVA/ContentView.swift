f2ab20cb92179aec12810f13b1a1f3a3c2670f2b
Luiz Felipe <aguiarpassaroni@gmail.com>
2026-02-16 13:14:18 -0300

    Initial Commit

---
+++ Mais.UVA/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Mais.UVA
+//
+//  Created by LUIZ PASSARONI on 16/02/26.
+//  Copyright © 2026 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mais.UVA/Mais_UVAApp.swift
@@ -0,0 +1,18 @@
+//
+//  Mais_UVAApp.swift
+//  Mais.UVA
+//
+//  Created by LUIZ PASSARONI on 16/02/26.
+//  Copyright © 2026 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Mais_UVAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
